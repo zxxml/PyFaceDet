906cc5b2df6d1a1aab378d052942b22ce43c9386
zxxml <zxxml@outlook.com>
2019-07-04 11:04:39 +0800

    Usable for Windows 10 and Ubuntu 18.04 x86_64 in a single thread.

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # PyFace
 PyFace is a Python wrapper of libfacedetection. PyFace 是 libfacedetection 人性化的 Python 封装。
+
+## 中文文档
+
+### 作用
+
+PyFace 旨在提供跨平台的开箱即用的人脸检测 API。
+
+### 优点
+
+- 跨平台（已在 Windows 10 x86_64 和 Ubuntu 18.04 x86_64 测试通过）
+- 开箱即用（提供单一的 API，它可以接收不同类型的输入，并且返回统一的结果）
+- 依赖项少（使用 `ctypes` 封装，只因功能需求引入 `pillow` 和 `numpy` 两个外部依赖）
+- 性能损耗少（一定程度上优化了对 Python 数据结构的处理，还将继续进行优化）
+
+### 使用方式
+
+将本项目存放到你喜欢的位置，然后想办法引用其中的 `facedetectcnn.py` 文件，该文件提供的 API 如下所示。
+
+```python
+facedetect_cnn(image: Union[str, Path, Image, np.ndarray, bytes], width: int = 0, height: int = 0, step: int = 0) -> Faces
+```
+
+你可以根据自己的需要传入图像路径（以字符串或 `Path` 对象的形式）、`PIL Image` 对象、BGR 格式的 `numpy` 数组或 `bytes` 对象。
+
+当你传入 `PIL Image` 对象时，你可以选择同时提供 `width` 和 `height` 参数以修改传入 libfacedetection 的图像尺寸（请注意这还将**影响所得的结果**）。
+
+当你传入 `bytes` 对象时，你必须同时提供 `width`、`height` 和 `step` 参数，它们分别代表图像的宽度、高度和每行字节数（通常为宽度的 3 倍）。
+
+### 开发计划
+
+- [ ] 开箱即用的多线程支持
+- [ ] 树莓派支持（Raspbian）
+- [ ] 更多的 Linux 发行版测试
+
+## English
+
+TODO
---
+++ facedetectcnn.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env/python3
+# -*- coding: utf-8 -*-
+import ctypes
+import platform
+from pathlib import Path
+from typing import List, Tuple, Union
+
+import numpy as np
+from PIL import Image as PIL
+from PIL.Image import Image
+
+if platform.system() == 'Windows':
+    # load .dll for Windows
+    LoadDLL = ctypes.WinDLL
+else:
+    # load lib for Linux, Darwin, etc
+    LoadDLL = ctypes.cdll.LoadLibrary
+
+arch = platform.architecture()[0]
+if platform.system() == 'Windows':
+    # this dll is compiled from modified code
+    # which keeps the function name unmangled
+    # the dll is compiled in VS 2015 for x86
+    # with ENABLE_AVX2 option on to speed up
+    current_dir = Path(__file__).parent
+    dll_name = 'libfacedetection_{0}.{1}'
+    dll_name = dll_name.format(arch, 'dll')
+    path = Path(current_dir / dll_name)
+    dll = LoadDLL(path.resolve().as_posix())
+elif platform.system() == 'Linux':
+    # the dll the compiled using gcc for x86
+    # with ENABLE_AVX2 option on to speed up
+    current_dir = Path(__file__).parent
+    dll_name = 'libfacedetection_{0}.{1}'
+    dll_name = dll_name.format(arch, 'so')
+    path = Path(current_dir / dll_name)
+    dll = LoadDLL(path.resolve().as_posix())
+elif platform.system() == 'Darwin':
+    # TODO: Darwin support
+    raise NotImplementedError
+else:
+    # TODO: BSD support, etc
+    raise NotImplementedError
+
+# THE SIGNATURE OF facedetect_cnn
+# unsigned char * result_buffer
+# unsigned char * rgb_image_data
+# int width, int height, int step
+dll.facedetect_cnn.restype = ctypes.POINTER(ctypes.c_int)
+dll.facedetect_cnn.argtypes = [ctypes.POINTER(ctypes.c_ubyte),
+                               ctypes.POINTER(ctypes.c_ubyte),
+                               ctypes.c_int, ctypes.c_int, ctypes.c_int]
+
+Faces = List[Tuple[int, int, int, int, int]]
+c_ubyte_p = ctypes.POINTER(ctypes.c_ubyte)
+c_short_p = ctypes.POINTER(ctypes.c_short)
+c_int_p = ctypes.POINTER(ctypes.c_int)
+
+
+# noinspection PyTypeChecker
+# noinspection PyUnresolvedReferences
+# noinspection PyCallingNonCallable
+def cfacedetect_cnn(image: bytes,
+                    width: int,
+                    height: int,
+                    step: int) -> Faces:
+    image_type = ctypes.c_ubyte * (step * height)
+    image_data = image_type.from_buffer_copy(image)
+    # the magic number 0x20000 is DETECT_BUFFER_SIZE
+    # which used in the original example source code
+    result_buffer = (ctypes.c_ubyte * 0x20000)()
+    # the return value is (int *)(result_buffer)
+    # so just ignore it and use the result_buffer
+    dll.facedetect_cnn(result_buffer, image_data,
+                       width, height, step)
+    length = ctypes.cast(result_buffer, c_int_p)[0]
+    faces = ctypes.cast(result_buffer, c_short_p)
+    faces_results = []
+    for i in range(length):
+        start_addr = 2 + 142 * i
+        x = faces[start_addr]
+        y = faces[start_addr + 1]
+        w = faces[start_addr + 2]
+        h = faces[start_addr + 3]
+        c = faces[start_addr + 4]
+        a = faces[start_addr + 5]
+        result = (x, y, w, h, c, a)
+        faces_results.append(result)
+    # since here is Python
+    # no more length needed
+    return faces_results
+
+
+# noinspection PyTypeChecker
+def facedetect_cnn(image: Union[str, Path, Image, np.ndarray, bytes],
+                   width: int = 0, height: int = 0, step: int = 0) -> Faces:
+    # if the image is filepath(str)
+    # then build the absolute path
+    if isinstance(image, str):
+        image = Path(image)
+        image = image.resolve()
+    # if the given type is Path
+    # then open the image by it
+    if isinstance(image, Path):
+        image = image.as_posix()
+        image = PIL.open(image)
+    # if the given type is PIL Image
+    # then convert it from RGB to BGR
+    if isinstance(image, Image):
+        # if there is size given
+        # then resize the image
+        if width and height:
+            size = (width, height)
+            image = image.resize(size)
+        image = image.convert('RGB')
+        image = np.array(image)
+        image = image[..., ::-1]
+    # if the given type is numpy array
+    # then calculate all the parameters
+    if isinstance(image, np.ndarray):
+        width = image.shape[1]
+        height = image.shape[0]
+        depth = image.shape[2]
+        step = width * depth
+        image = image.tobytes()
+    # everything is ready, call the function for result
+    return cfacedetect_cnn(image, width, height, step)
+
+
+if __name__ == '__main__':
+    # print(dll.facedetect_cnn)
+    facedetect_cnn('lena512color.tiff')
